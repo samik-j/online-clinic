f44013dfea2d948af65376ba597b555e141a3638
joanna <jo.senczuk@gmail.com>
2017-12-13 13:31:37 +0000

    Initialize project

---
+++ src/main/java/com/joanna/onlineclinic/OnlineclinicApplication.java
@@ -0,0 +1,12 @@
+package com.joanna.onlineclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineclinicApplication.class, args);
+	}
+}
---
+++ src/test/java/com/joanna/onlineclinic/OnlineclinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.joanna.onlineclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlineclinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
