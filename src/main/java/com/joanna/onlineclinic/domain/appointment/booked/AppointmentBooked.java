02d0dc647b5ee8f892d46001a75d1d982284687d
joanna <jo.senczuk@gmail.com>
2017-12-14 18:22:55 +0000

    Override equals and hashCode for AppointmentBooked

--- src/main/java/com/joanna/onlineclinic/domain/appointment/booked/AppointmentBooked.java
+++ src/main/java/com/joanna/onlineclinic/domain/appointment/booked/AppointmentBooked.java
@@ -60,4 +60,23 @@ public class AppointmentBooked {
     public boolean isConfirmed() {
         return confirmed;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        AppointmentBooked that = (AppointmentBooked) o;
+
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
 }
