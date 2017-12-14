1e3be4d8eb9ed93a6a314acb26a6a73caac4b217
joanna <jo.senczuk@gmail.com>
2017-12-14 17:37:04 +0000

    Add tests for Patient

---
+++ src/test/java/com/joanna/onlineclinic/domain/patient/PatientTest.java
@@ -0,0 +1,23 @@
+package com.joanna.onlineclinic.domain.patient;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PatientTest {
+
+    @Test
+    public void shouldCreatePatient() {
+        // when
+        Patient patient = new Patient("First", "Last", "1234567890", "07522222222", "fake@gmail.com");
+
+        // then
+        assertEquals("First", patient.getFirstName());
+        assertEquals("Last", patient.getLastName());
+        assertEquals("1234567890", patient.getNHSNumber());
+        assertEquals("07522222222", patient.getPhoneNumber());
+        assertEquals("fake@gmail.com", patient.getEmail());
+        assertTrue(patient.getAppointments().isEmpty());
+    }
+}
