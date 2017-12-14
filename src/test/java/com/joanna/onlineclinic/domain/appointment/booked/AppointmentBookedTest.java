6e4b5f362691874e1fb1b3505dee24d4883b8eab
joanna <jo.senczuk@gmail.com>
2017-12-14 18:29:20 +0000

    Add tests for AppointmentBooked

---
+++ src/test/java/com/joanna/onlineclinic/domain/appointment/booked/AppointmentBookedTest.java
@@ -0,0 +1,33 @@
+package com.joanna.onlineclinic.domain.appointment.booked;
+
+import com.joanna.onlineclinic.domain.doctor.Doctor;
+import com.joanna.onlineclinic.domain.doctor.Specialty;
+import com.joanna.onlineclinic.domain.patient.Patient;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+
+public class AppointmentBookedTest {
+
+    @Test
+    public void shouldCreateAppointment() {
+        // given
+        Doctor doctor = new Doctor("First", "Last", Specialty.PEDIATRICIAN);
+        LocalDateTime appointmentDateTime = LocalDateTime.of(2017, 9, 12, 18, 00);
+        Patient patient = new Patient("First", "Last", "1234567890", "07522222222", "fake@gmail.com");
+
+        // when
+        AppointmentBooked appointment = new AppointmentBooked(doctor, appointmentDateTime, patient, "Sick");
+
+        // then
+        assertEquals(doctor, appointment.getDoctor());
+        assertEquals(appointmentDateTime, appointment.getAppointmentDateTime());
+        assertEquals(patient, appointment.getPatient());
+        assertEquals("Sick", appointment.getReason());
+        assertFalse(appointment.isConfirmed());
+    }
+}
