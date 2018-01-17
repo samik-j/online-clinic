7704f24880dfa1376e96206a8595e224707878f5
joanna <jo.senczuk@gmail.com>
2018-01-17 13:48:25 +0000

    Update DoctorController with necessary fields

--- src/main/java/com/joanna/onlineclinic/web/doctor/DoctorController.java
+++ src/main/java/com/joanna/onlineclinic/web/doctor/DoctorController.java
@@ -1,9 +1,19 @@
 package com.joanna.onlineclinic.web.doctor;
 
+import com.joanna.onlineclinic.domain.doctor.DoctorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/doctors")
 public class DoctorController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DoctorController.class);
+    private DoctorService service;
+
+    public DoctorController(DoctorService service) {
+        this.service = service;
+    }
 }
