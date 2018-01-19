63ea147a59f0c36b5fde180351d48770f714da25
joanna <jo.senczuk@gmail.com>
2018-01-19 17:16:21 +0000

    Create PatienrResource

---
+++ src/main/java/com/joanna/onlineclinic/web/patient/PatientResource.java
@@ -0,0 +1,62 @@
+package com.joanna.onlineclinic.web.patient;
+
+public class PatientResource {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String NHSNumber;
+    private String phoneNumber;
+    private String email;
+
+    public PatientResource() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNHSNumber() {
+        return NHSNumber;
+    }
+
+    public void setNHSNumber(String NHSNumber) {
+        this.NHSNumber = NHSNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
