71e9d3e0e99a40dc9f6d4c7f1b86d0a74f37827e
joanna <jo.senczuk@gmail.com>
2018-02-01 15:38:26 +0000

    Add tests for Appointment

--- src/test/java/com/joanna/onlineclinic/domain/appointment/AppointmentTest.java
+++ src/test/java/com/joanna/onlineclinic/domain/appointment/AppointmentTest.java
@@ -8,6 +8,9 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AppointmentTest {
 
@@ -25,6 +28,34 @@ public class AppointmentTest {
         assertEquals(doctor, appointment.getDoctor());
         assertEquals(date, appointment.getDate());
         assertEquals(time, appointment.getTime());
+        assertTrue(appointment.isAvailable());
     }
 
+    @Test
+    public void shouldBook() {
+        // given
+        Doctor doctor = new Doctor("First", "Last", Specialty.PEDIATRICIAN);
+        LocalDate date = LocalDate.of(2017, 9, 12);
+        LocalTime time = LocalTime.of(18, 00);
+        Appointment appointment = new Appointment(doctor, date, time);
+
+        // when
+        appointment.book();
+
+        // then
+        assertFalse(appointment.isAvailable());
+    }
+
+    @Test
+    public void bookShouldThrowExceptionIfIsNotAvailable() {
+        // given
+        Doctor doctor = new Doctor("First", "Last", Specialty.PEDIATRICIAN);
+        LocalDate date = LocalDate.of(2017, 9, 12);
+        LocalTime time = LocalTime.of(18, 00);
+        Appointment appointment = new Appointment(doctor, date, time);
+        appointment.book();
+
+        // then
+        assertThrows(IncorrectAppointmentAvailabilityException.class, () -> appointment.book());
+    }
 }
