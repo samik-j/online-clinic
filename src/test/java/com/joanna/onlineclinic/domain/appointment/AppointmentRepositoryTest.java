0aa3e8cd2e40bf3f1a194128ebd0edd19d94bc1b
joanna <jo.senczuk@gmail.com>
2018-02-01 23:33:56 +0000

    test

---
+++ src/test/java/com/joanna/onlineclinic/domain/appointment/AppointmentRepositoryTest.java
@@ -0,0 +1,94 @@
+package com.joanna.onlineclinic.domain.appointment;
+
+import com.joanna.onlineclinic.domain.doctor.Doctor;
+import com.joanna.onlineclinic.domain.doctor.DoctorRepository;
+import com.joanna.onlineclinic.domain.doctor.Specialty;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class AppointmentRepositoryTest {
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    @Before
+    public void createDoctors() {
+        Doctor doctor1 = new Doctor("First", "Last", Specialty.PEDIATRICIAN);
+        Doctor doctor2 = new Doctor("First2", "Last2", Specialty.DERMATOLOGIST);
+        doctorRepository.save(doctor1);
+        doctorRepository.save(doctor2);
+    }
+
+    @After
+    public void deleteDoctors() {
+        doctorRepository.deleteAll();
+    }
+
+
+    @Test
+    public void shouldReturnTrueIfAppointmentExists() {
+        // given
+        LocalDate date = LocalDate.of(2017, 9, 12);
+        LocalTime time = LocalTime.of(18, 00);
+        Appointment appointment = new Appointment(doctorRepository.findOne(1L), date, time);
+
+        appointmentRepository.save(appointment);
+
+        // when
+        boolean result = appointmentRepository.existsByDoctorIdAndAndDateAndTime(1L, date, time);
+        // then
+        assertTrue(result);
+    }
+
+    @Test
+    public void shouldReturnFalseIfAppointmentDoesNotExist() {
+        // given
+        LocalDate date = LocalDate.of(2017, 9, 12);
+        LocalTime time = LocalTime.of(18, 00);
+
+        // when
+        boolean result = appointmentRepository.existsByDoctorIdAndAndDateAndTime(1L, date, time);
+
+        // then
+        assertFalse(result);
+    }
+
+    @Test
+    public void shouldFindAllByDoctorId() {
+        // given
+
+        LocalDate date = LocalDate.of(2017, 9, 12);
+        LocalTime time = LocalTime.of(18, 00);
+        Appointment appointment1 = new Appointment(doctorRepository.findOne(1L), date, time);
+        Appointment appointment2 = new Appointment(doctorRepository.findOne(2L), date, time);
+
+        appointmentRepository.save(appointment1);
+        appointmentRepository.save(appointment2);
+
+        // when
+        List<Appointment> found = appointmentRepository.findAll(1L);
+
+        // then
+        assertEquals(1, found.size());
+        assertTrue(found.contains(appointment1));
+    }
+
+}
