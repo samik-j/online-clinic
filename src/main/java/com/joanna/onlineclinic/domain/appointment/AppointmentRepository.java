1c4fb3cdbe4e75c5b54c89d8e84669f357e99b13
joanna <jo.senczuk@gmail.com>
2018-02-02 15:12:34 +0000

    Refactor findAvailable query

--- src/main/java/com/joanna/onlineclinic/domain/appointment/AppointmentRepository.java
+++ src/main/java/com/joanna/onlineclinic/domain/appointment/AppointmentRepository.java
@@ -22,8 +22,8 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Long> 
     @Query("SELECT appointment FROM Appointment appointment WHERE " +
             "appointment.doctor.id = :doctorId AND " +
             "appointment.available = true AND " +
-            "appointment.date > :date AND " +
-            "appointment.time > :time")
+            "appointment.date > :date OR " +
+            "(appointment.date = :date AND appointment.time > :time)")
     List<Appointment> findAvailable(
             @Param("doctorId") long doctorId,
             @Param("date") LocalDate date,
